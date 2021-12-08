b11169653edf7f8c844428cc7fb137d2a88a9c7d
kodezon <kode.zon@gmail.com>
2021-12-09 01:46:55 +0700

    remove custom module (it blocks compile)

--- custom-modules/bpmn-js-custom-renderer/index.js
+++
@@ -1,8 +0,0 @@
-import CustomBpmnRenderer from './CustomBpmnRenderer';
-import CustomBpmnTextRenderer from './CustomBpmnTextRenderer';
-
-export default {
-    __init__: ['customBpmnRenderer', 'customBpmnTextRenderer'],
-    customBpmnRenderer: [ 'type', CustomBpmnRenderer ],
-    customBpmnTextRenderer: [ 'type', CustomBpmnTextRenderer ]
-}
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -4,7 +4,7 @@ declare module 'bpmn-js-color-picker';
 declare module 'bpmn-js-properties-panel';
 declare module 'bpmn-js-properties-panel/lib/provider/bpmn';
 declare module 'bpmn-js-task-resize/lib';
-declare module 'custom-modules/bpmn-js-custom-renderer';
+//declare module 'custom-modules/bpmn-js-custom-renderer';
 declare module 'bpmn-js-differ';
 declare module 'bpmn-moddle';
 declare module 'jquery';
