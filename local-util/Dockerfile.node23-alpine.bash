b572930718ebb9acec6b76d4eb99f0d719a7202b
kode-zon <kode.zon@gmail.com>
2024-12-26 01:00:53 +0700

    add envsubst to playground docker

--- local-util/Dockerfile.node23-alpine.bash
+++ local-util/Dockerfile.node23-alpine.bash
@@ -2,6 +2,7 @@ from node:23.4.0-alpine3.21
 
 RUN apk add --no-cache bash && \
     apk add --no-cache openssl && \
+    apk add --no-cache envsubst && \
     npm i -g @angular/cli
 
 WORKDIR /etc/share/project/
