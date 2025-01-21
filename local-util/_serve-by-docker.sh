ce85a936991a68c32e999dc2aa1f24cae12ccfa0
kode-zon <kode.zon@gmail.com>
2025-01-21 22:52:09 +0700

    shell command to serve using container

--- local-util/_serve-by-docker.sh
+++ local-util/_serve-by-docker.sh
@@ -3,18 +3,11 @@
 THIS_SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $THIS_SCRIPT_DIR/__path-docker.sh
 
-docker run -d --rm --name nginx-bpmn-js-app \
+containerId=$(docker run -d --rm --name nginx-bpmn-js-app \
                         -p 80:80 \
                         -v "$ENTRY_DIR/local-util/nginx-template.conf:/etc/nginx/nginx-template.conf:ro" \
-                        -v "$DOCKER_ENTRY_DIR/dist/bpmn-js-app/browser:/usr/share/nginx/html:ro" \
-                        nginx:stable-alpine3.20-slim 
-
-# containerId=$(docker run -d --name nginx-bpmn-js-app \
-#                         -p 80:80 \
-#                         -v "$ENTRY_DIR/local-util/nginx-template.conf:/etc/nginx/nginx-template.conf:ro" \
-#                         -v "$DOCKER_ENTRY_DIR/dist/bpmn-js-app/browser:/usr/share/nginx/html:ro" \
-#                         nginx:stable-alpine3.20-slim \
-#                         nginx -c /etc/nginx/nginx-template.conf)
+                        -v "$DOCKER_ENTRY_DIR/dist/bpmn-js-app:/usr/share/nginx/html:ro" \
+                        nginx:stable-alpine3.20-slim )
 
 echo "container id : $containerId"
 docker logs $containerId
